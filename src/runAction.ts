0a763386a58f1bf7631fec31e44ece33f7f4dcb1
hertz <georgedot@gmail.com>
2020-10-21 04:24:06 +0400

    feat: abort if no pr

--- src/runAction.ts
+++ src/runAction.ts
@@ -5,6 +5,7 @@ import {upsertCommentByLogin} from './comment'
 import {compose} from './message'
 import {npmPack} from './goggles/npmPack'
 import {npmInstall} from './goggles/npmInstall'
+import {info} from '@actions/core'
 
 export const runAction = async (
   octokit: ReturnType<typeof getOctokit>,
@@ -23,14 +24,18 @@ export const runAction = async (
     buildVersion(context, resolvedPackageJsonPath)
   ])
 
-  await upsertCommentByLogin(
-    octokit,
-    context.repo,
-    login,
-    issue_number,
-    compose(context, nextVersion.short, [
-      await npmPack(resolvedPackageRoot),
-      ...(await npmInstall(resolvedPackageRoot))
-    ])
-  )
+  if (issue_number == null) {
+    info('Pull request not detected... Abort')
+  } else {
+    await upsertCommentByLogin(
+      octokit,
+      context.repo,
+      login,
+      issue_number,
+      compose(context, nextVersion.short, [
+        await npmPack(resolvedPackageRoot),
+        ...(await npmInstall(resolvedPackageRoot))
+      ])
+    )
+  }
 }
